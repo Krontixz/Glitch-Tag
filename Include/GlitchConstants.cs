e96d80c63c2af6f5217d3ed738e0c4f2150690c1
Krontixz <zaki.coding0119@gmail.com>
2026-04-29 19:35:17 -0700

    Create GlitchConstants.cs

---
+++ Include/GlitchConstants.cs
@@ -0,0 +1,10 @@
+public static class GlitchConstants {
+    public const int SCREEN_WIDTH = 1920;
+    public const int SCREEN_HEIGHT = 1080;
+    public const float SPLASH_DURATION = 3.0f;
+    public const int MAX_USERNAME_LENGTH = 16;
+    public const int MIN_USERNAME_LENGTH = 3;
+    public const string ASSET_PATH_LOGO = "Assets/Logo/Logo.png";
+    public const string ASSET_PATH_TOS = "Assets/Legal/Protocols.txt";
+    public const string DLL_PATH_SECURITY = "glitch_security.dll";
+}
